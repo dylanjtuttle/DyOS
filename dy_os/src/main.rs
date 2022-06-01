8c2a594a623a05621e3b62d047f09140d3dc0168
Dylan Tuttle <jdylantuttle@gmail.com>
2022-05-31 21:35:55 -0600

    Added missing comments

--- dy_os/src/main.rs
+++ dy_os/src/main.rs
@@ -8,15 +8,20 @@ static HELLO: &[u8] = b"Hello, world!";
 // This function is the entry point, since the linker looks for a function named _start by default
 #[no_mangle]  // Ensure that the Rust compiler actually makes the name of this function _start
 pub extern "C" fn _start() -> ! {
+    // Cast 0xb8000 into a raw pointer
     let vga_buffer = 0xb8000 as *mut u8;
 
+    // Iterate through the bytes in the "Hello, world!" byte string
     for (i, &byte) in HELLO.iter().enumerate() {
         unsafe {
+            // Write the current byte to the screen
             *vga_buffer.offset(i as isize * 2) = byte;
+            // Set the color of this byte to cyan (0xB)
             *vga_buffer.offset(i as isize * 2 + 1) = 0xB;
         }
     }
 
+    // Loop forever (this is still an OS, so we only want to exit when the computer is turned off)
     loop {}
 }
 
